4d1571a3605d0075126f8f0d9c5f933f760347e3
PenneyGadget <dreamofgreen@gmail.com>
2016-03-16 17:02:13 -0600

    Filter/analyze solution

--- analyze.js
+++ analyze.js
@@ -1,9 +1,36 @@
 var _= require("lodash");
 
-var freelancers =  [ { name: "mike",  income: 2563 },
+var freelancers =  [
+                     { name: "mike",  income: 2563 },
                      { name: "kim",   income: 1587 },
                      { name: "liz",   income: 3541 },
                      { name: "tom",   income: 2475 },
                      { name: "bello", income: 987  },
                      { name: "frank", income: 2975 }
                    ];
+
+var freelancerStats = function(freelancers) {
+  var totalIncome = _.reduce(freelancers, function(sum, freelancer) {
+    return sum + freelancer.income;
+  }, 0);
+
+  var averageIncome = totalIncome / freelancers.length;
+
+  var sorted = _.sortBy(freelancers, 'income');
+
+  var underperform = _.filter(sorted, function(freelancer) {
+    return freelancer.income <= averageIncome;
+  });
+
+  var overperform = _.filter(sorted, function(freelancer) {
+    return freelancer.income > averageIncome;
+  });
+
+  return {
+    average: averageIncome,
+    underperform: underperform,
+    overperform: overperform
+  };
+};
+
+module.exports = freelancerStats;
